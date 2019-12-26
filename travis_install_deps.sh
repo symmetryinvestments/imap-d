67f502e633bce64c9d911624320924e7cee3b8be
Laeeth Isharc <laeeth@kaleidic.io>
2019-12-26 17:20:08 +0000

    escape $ in sh

--- travis_install_deps.sh
+++ travis_install_deps.sh
@@ -9,17 +9,17 @@ sudo make install
 popd
 cat > libssl.pc << EOF
 prefix=/usr/local/openssl
-exec_prefix=${prefix}
-libdir=${exec_prefix}/lib
-sharedlibdir=${libdir}
-includedir=${prefix}/include
+exec_prefix=\${prefix}
+libdir=\${exec_prefix}/lib
+sharedlibdir=\${libdir}
+includedir=\${prefix}/include
 
 Name: OpenSSL-libssl
 Description: Secure Sockets Layer and cryptography libraries
 Version: 1.1.1
 Requires.private: libcrypto
-Libs: -L${libdir} -L${sharedlibdir} -lssl
-Cflags: -I${includedir}
+Libs: -L\${libdir} -L\${sharedlibdir} -lssl
+Cflags: -I\${includedir}
 EOF
 sudo mv libssl.pc /usr/lib/x86_64-linux-gnu/pkgconfig/libssl.pc
 

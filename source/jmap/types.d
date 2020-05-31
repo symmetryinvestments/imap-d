3a2d4c278dc80a7ab7dfa9b41ea58933dd959218
Laeeth Isharc <laeeth@kaleidic.io>
2020-05-31 20:52:09 +0100

    basic types work

--- source/jmap/types.d
+++ source/jmap/types.d
@@ -1,6 +1,10 @@
+module jmap.types;
 import std.datetime : SysTime;
 import std.typecons : Nullable;
+import kaleidic.sil.lang.types : Variable,Function,SILdoc;
+import asdf;
 
+alias Url = string;
 alias Emailer = string;
 alias Attachment = ubyte[];
 alias ModSeq = ulong;
@@ -13,11 +17,55 @@ struct SessionCoreCapabilities
 	uint maxSizeUpload;
 	uint maxConcurrentUpload;	
 	uint maxSizeRequest;
-	uint maxConcurrentRequest;
+	uint maxConcurrentRequests;
 	uint maxCallsInRequest;
 	uint maxObjectsInGet;
 	uint maxObjectsInSet;
 	string[] collationAlgorithms;
+}
+																															    
+															 
+@("urn:ietf:params:jmap:mail")
+enum EmailQuerySortOption
+{
+	receivedAt,
+	from,
+	to,
+	subject,
+	size,
+
+	@serializationKeys("header.x-spam-score")
+	headerXSpamScore,
+}
+
+struct AccountParams
+{
+	//EmailQuerySortOption[] emailQuerySortOptions;
+	string[] emailQuerySortOptions;
+	Nullable!int maxMailboxDepth;
+	Nullable!int maxMailboxesPerEmail;
+	Nullable!int maxSizeAttachmentsPerEmail;
+	Nullable!int maxSizeMailboxName;
+	bool mayCreateTopLevelMailbox;
+}
+
+struct SubmissionParams
+{
+   int maxDelayedSend;
+   string[] submissionExtensions;
+}
+
+struct AccountCapabilities
+{
+	@serializationKeys("urn:ietf:params:jmap:mail")
+	AccountParams accountParams;
+
+	@serializationKeys("urn:ietf:params:jmap:submission")
+	SubmissionParams submissionParams;
+
+	//private Asdf vacationResponseParams;
+
+	//private Asdf[string] extraAccountCapabilities;
 }
 
 struct Account
@@ -25,22 +73,38 @@ struct Account
 	string name;
 	bool isPersonal;
 	bool isReadOnly;
-	Variable[string] accountCapabilities;
+
+	bool isArchiveUser = false;
+	AccountCapabilities accountCapabilities;
 	string[string] primaryAccounts;
 }
 
 struct Session
 {
-	SessionCoreCapabilities capabilities;
+	SessionCoreCapabilities coreCapabilities;
+
 	Account[string] accounts;
 	string[string] primaryAccounts;
 	string username;
-	string apiUrl;
-	string downloadUrl;
-	string uploadUrl;
-	string eventSourceUrl;
+	Url apiUrl;
+	Url downloadUrl;
+	Url uploadUrl;
+	Url eventSourceUrl;
 	string state;
-	Variable[string] otherProperties;
+	private Asdf[string] capabilities;
+
+	void finalizeDeserialization(Asdf data)
+	{
+		import asdf : deserialize, Asdf;
+		import std.stdio : writeln;
+
+		foreach(el;data["capabilities"].byKeyValue)
+			capabilities[el.key] = el.value.get!Asdf(Asdf.init);
+		//writeln(data);
+		//writeln(data["capabilities"]["urn:ietf:params:jmap:core"]);
+		//writeln(capabilities["urn:ietf:params:jmap:core"]);
+		this.coreCapabilities = deserialize!SessionCoreCapabilities(capabilities["urn:ietf:params:jmap:core"]);
+	}
 }
 
 struct Email
@@ -150,3 +214,67 @@ struct HighLowModSeqCache
 	ModSeq lowModSeqMailbox;
 }
 
+/+
+{
+   "accounts" : {
+      "u1f4140ae" : {
+         "accountCapabilities" : {
+            "urn:ietf:params:jmap:mail" : {
+               "emailQuerySortOptions" : [
+                  "receivedAt",
+                  "from",
+                  "to",
+                  "subject",
+                  "size",
+                  "header.x-spam-score"
+               ],
+               "maxMailboxDepth" : null,
+               "maxMailboxesPerEmail" : 1000,
+               "maxSizeAttachmentsPerEmail" : 50000000,
+               "maxSizeMailboxName" : 490,
+               "mayCreateTopLevelMailbox" : true
+            },
+            "urn:ietf:params:jmap:submission" : {
+               "maxDelayedSend" : 44236800,
+               "submissionExtensions" : []
+            },
+            "urn:ietf:params:jmap:vacationresponse" : {}
+         },
+         "isArchiveUser" : false,
+         "isPersonal" : true,
+         "isReadOnly" : false,
+         "name" : "laeeth@kaleidic.io"
+      }
+   },
+   "apiUrl" : "https://jmap.fastmail.com/api/",
+   "capabilities" : {
+      "urn:ietf:params:jmap:core" : {
+         "collationAlgorithms" : [
+            "i;ascii-numeric",
+            "i;ascii-casemap",
+            "i;octet"
+         ],
+         "maxCallsInRequest" : 64,
+         "maxConcurrentRequests" : 10,
+         "maxConcurrentUpload" : 10,
+         "maxObjectsInGet" : 1000,
+         "maxObjectsInSet" : 1000,
+         "maxSizeRequest" : 10000000,
+         "maxSizeUpload" : 250000000
+      },
+      "urn:ietf:params:jmap:mail" : {},
+      "urn:ietf:params:jmap:submission" : {},
+      "urn:ietf:params:jmap:vacationresponse" : {}
+   },
+   "downloadUrl" : "https://jmap.fastmail.com/download/{accountId}/{blobId}/{name}",
+   "eventSourceUrl" : "https://jmap.fastmail.com/event/",
+   "primaryAccounts" : {
+      "urn:ietf:params:jmap:mail" : "u1f4140ae",
+      "urn:ietf:params:jmap:submission" : "u1f4140ae",
+      "urn:ietf:params:jmap:vacationresponse" : "u1f4140ae"
+   },
+   "state" : "cyrus-12046746;p-5;vfs-0",
+   "uploadUrl" : "https://jmap.fastmail.com/upload/{accountId}/",
+   "username" : "laeeth@kaleidic.io"
+}
++/

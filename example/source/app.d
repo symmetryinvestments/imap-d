763d157a16e5f59e477154e30b27dc169e6020c8
Toby Hutton <thutton@symmetryinvestments.com>
2020-10-01 18:28:12 +1000

    Fix the D example code slightly.
    - Add a more informative error message when the auth credentials are
      missing from the environment.
    - Remove the '/' prefix from the mailbox.  Dovecot doesn't like it.
    - Prevent a crash when trying to reference values in a potentially empty
      search result.

--- example/source/app.d
+++ example/source/app.d
@@ -3,7 +3,7 @@ import imap;
 int main(string[] args)
 {
 	import std.conv : to;
-	import std.stdio : writeln, stderr, writefln;
+	import std.stdio : writeln, stderr;
 	import std.range : back;
 	import std.process : environment;
 	import std.string  : join;
@@ -11,7 +11,6 @@ int main(string[] args)
 
 	if (args.length != 4)
 	{
-		import std.stdio: stderr;
 		stderr.writeln("imap-example <server> <port> <mailbox>");
 		return -1;
 	}
@@ -22,10 +21,9 @@ int main(string[] args)
 	auto user = environment.get("IMAP_USER","");
 	auto pass = environment.get("IMAP_PASS","");
 
-	if (user.length ==0 || pass.length == 0)
+	if (user.length == 0 || pass.length == 0)
 	{
-		stderr.writeln("./imap-example <server> <port> <mailbox>\n");
-		stderr.writeln("eg ./imap-example imap.fastmail.com 993 INBOX");
+		stderr.writeln("IMAP_USER and IMAP_PASS environment variables must be set.");
 		return -1;
 	}
 
@@ -42,27 +40,34 @@ int main(string[] args)
 	session = session.login();
 
 	// Select Inbox
-	auto INBOX =Mailbox(mailbox,"/",'/');
+	auto INBOX = Mailbox(mailbox,"",'/');
 	auto result = session.select(INBOX);
 
 	// search all messages since 29 Jan 2019 and get UIDs using raw query interface
 	auto searchResult = session.search("SINCE 29-Jan-2019");
+	writeln("--- Raw 'SINCE 29-Jan-2019' search results:");
 	writeln(searchResult.value);
-	writeln(searchResult.ids);
+	writeln("--- Search result IDs: ", searchResult.ids);
 
 	// search all messages from GitHub since 29 Jan 2019 and get UIDs using high level query interface
 	SearchQuery query = {since:Date(2019,1,29),fromContains:"GitHub"};
 	searchResult = session.searchQuery("INBOX",query);
+	writeln("--- Structured 'since:Date(2019,1,29),fromContains:\"GitHub\"' search results:");
 	writeln(searchResult.value);
 
 	// fetch one of the messages from above
-	auto messageResult = session.fetchText(searchResult.ids.back.to!string);
-	writeln(messageResult.value);
+	if (searchResult.ids.length > 0)
+	{
+		auto exampleId = searchResult.ids.back.to!string;
+		auto messageResult = session.fetchText(exampleId);
+		writeln("--- Message text for ID ", exampleId, ":");
+		writeln(messageResult.value);
 
-	// just fetch the fields we care about
-	auto relevantFields = [ "FROM", "TO" ];
-	auto fieldsResult = session.fetchFields(searchResult.ids.back.to!string,relevantFields.join(" "));
-	writeln(fieldsResult.value);
+		// just fetch the fields we care about
+		auto relevantFields = [ "FROM", "TO" ];
+		auto fieldsResult = session.fetchFields(exampleId,relevantFields.join(" "));
+		writeln("--- Message 'from' and 'to' fields for ID ", exampleId, ":");
+		writeln(fieldsResult.value);
+	}
 	return 0;
 }
-
